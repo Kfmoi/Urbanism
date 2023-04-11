e35dda1f3633a80d37e007b266404cad06a7e266
Khalid <faragk1@mcmaster.ca>
2023-04-11 14:02:58 -0400

    F03: Fixed the Graph test cases to incorprate the new Graph implementation

--- pathfinder/src/test/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/graphADT/GraphImplTest.java
+++ pathfinder/src/test/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/graphADT/GraphImplTest.java
@@ -1,70 +1,102 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.graphADT;
 
-import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import ca.mcmaster.cas.se2aaa4.a4.pathfinder.graphADT.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
-
 
 public class GraphImplTest {
 
-    Graph<String,Double> graph;
+    private Graph graph = new GraphImpl(3);
+    
 
-
-    @BeforeEach
-    public void setUp() {
-        graph = new GraphImpl<>(5);
+    @Test
+    public void testAddNode() {
+        Node node = new NodeImpl(0.0, 0);
+        graph.addNode(node);
+        List<Node> nodes = graph.getNodes();
+        assertEquals(1, nodes.size());
+        assertEquals(node, nodes.get(0));
     }
-
+    
+    @Test
+    public void testAddEdge() {
+        Node nodeA  = new NodeImpl(0.0, 0);
+        Node nodeB = new NodeImpl(0.0, 1);
+        Edge edge = new EdgeImpl(0,1.0, nodeA.getIndex(), nodeB.getIndex());
+        graph.addEdge(edge);
+        List<Edge> edges = graph.getEdges();
+        assertEquals(1, edges.size());
+        assertEquals(edge, edges.get(0));
+    }
+    
     @Test
     public void testGetNumberOfNodes() {
-        Node<String,Double> nodeA = new NodeImpl<String,Double>("A", 0.0, 0);
-        Node<String,Double> nodeB = new NodeImpl<String,Double>("B", 0.0, 1);
-        graph.addNode(nodeA);
-        graph.addNode(nodeB);
-        assertEquals(2, graph.getNumberOfNodes());
+        assertEquals(0, graph.getNumberOfNodes());
+        Node node  = new NodeImpl(0.0, 0);
+        graph.addNode(node);
+        assertEquals(1, graph.getNumberOfNodes());
     }
-
+    
     @Test
     public void testGetNumberOfEdges() {
-        Node<String,Double> nodeA = new NodeImpl<String,Double>("A", 0.0, 0);
-        Node<String,Double> nodeB = new NodeImpl<String,Double>("B", 0.0, 1);
-        Edge<String,Double> edge = new EdgeImpl<>(nodeA, nodeB, 0.0, 0);
+        assertEquals(0, graph.getNumberOfEdges());
+        Node nodeA  = new NodeImpl(0.0, 0);
+        Node nodeB = new NodeImpl(0.0, 1);
+        Edge edge = new EdgeImpl(0,1.0, nodeA.getIndex(), nodeB.getIndex());
         graph.addEdge(edge);
         assertEquals(1, graph.getNumberOfEdges());
     }
-
+    
     @Test
     public void testGetNodes() {
-        Node<String,Double> nodeA = new NodeImpl<String,Double>("A", 0.0, 0);
-        Node<String,Double> nodeB = new NodeImpl<String,Double>("B", 0.0, 1);
+        List<Node> nodes = graph.getNodes();
+        assertTrue(nodes.isEmpty());
+        Node nodeA  = new NodeImpl(0.0, 0);
+        Node nodeB = new NodeImpl(0.0, 1);
         graph.addNode(nodeA);
         graph.addNode(nodeB);
-        assertEquals(List.of(nodeA, nodeB), graph.getNodes());
+        nodes = graph.getNodes();
+        assertEquals(2, nodes.size());
+        assertTrue(nodes.contains(nodeA));
+        assertTrue(nodes.contains(nodeB));
     }
-
+    
     @Test
     public void testGetEdges() {
-        Node<String,Double> nodeA = new NodeImpl<String,Double>("A", 0.0, 0);
-        Node<String,Double> nodeB = new NodeImpl<String,Double>("B", 0.0, 1);
-        Edge<String,Double> edge = new EdgeImpl<>(nodeA, nodeB, 0.0, 0);
+        List<Edge> edges = graph.getEdges();
+        assertTrue(edges.isEmpty());
+        Node nodeA  = new NodeImpl(0.0, 0);
+        Node nodeB = new NodeImpl(0.0, 1);
+        Edge edge = new EdgeImpl(0,1.0, nodeA.getIndex(), nodeB.getIndex());
         graph.addEdge(edge);
-        assertEquals(List.of(edge), graph.getEdges());
+        edges = graph.getEdges();
+        assertEquals(1, edges.size());
+        assertTrue(edges.contains(edge));
     }
-
+    
     @Test
-    public void testGetEdgeIndex() {
-        Node<String,Double> nodeA = new NodeImpl<String,Double>("A", 0.0, 0);
-        Node<String,Double> nodeB = new NodeImpl<String,Double>("B", 0.0, 1);
-        Edge<String,Double> edge = new EdgeImpl<>(nodeA, nodeB, 0.0, 0);
-        graph.addEdge(edge);
-        assertEquals(0, graph.getEdgeIndex(0, 1));
+    public void testGetNeighbours() {
+        Node nodeA = new NodeImpl(0.0, 0);
+        Node nodeB = new NodeImpl(0.0, 1);
+        Node nodeC = new NodeImpl(0.0, 2);
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+        graph.setNeighbours(0, Arrays.asList(1, 2));
+        List<Integer> neighbours = graph.getNeighbours(0);
+        assertEquals(2, neighbours.size());
+        assertTrue(neighbours.contains(1));
+        assertTrue(neighbours.contains(2));
     }
-
-
-
+    
 }
