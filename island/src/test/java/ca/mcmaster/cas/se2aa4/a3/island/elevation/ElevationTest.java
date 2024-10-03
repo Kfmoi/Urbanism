53b4f6bfff537f340558d8a711463482170da60a
Khalid Farag <faragk1@mcmaster.ca>
2024-10-03 10:42:47 -0400

    Change

--- island/src/test/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/ElevationTest.java
+++ island/src/test/java/ca/mcmaster/cas/se2aa4/a3/island/elevation/ElevationTest.java
@@ -1,7 +1,6 @@
 package ca.mcmaster.cas.se2aa4.a3.island.elevation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.util.TreeMap;
 import org.junit.jupiter.api.Test;
 
